7bb60977182e06bc221650cd7274ddc1862c95d1
Sérgio Teixeira <psat@users.noreply.github.com>
2015-06-16 23:19:00 +0100

    Javadoc
    Add missing javadoc

--- tests/com/psat/exercise/ConcreteMessageTest.java
+++ tests/com/psat/exercise/ConcreteMessageTest.java
@@ -2,17 +2,27 @@ package com.psat.exercise;
 
 import org.junit.Test;
 
+/**
+ * Class to test {@link ConcreteMessage} class
+ *
+ * @author Sérgio Teixeira
+ * @date 16/06/2015 23:16:44
+ */
 public class ConcreteMessageTest {
 
 	/**
-	 * Test that a {@link ConcreteMessage} object cannot created with a
-	 * <code>null</code> {@link ResourceScheduler} object or a group id
+	 * Test that a {@link ConcreteMessage} object cannot be created with a
+	 * <code>null</code> {@link ResourceScheduler} object
 	 */
 	@Test(expected = NullPointerException.class)
 	public void testConcreteMessageCreationWithNullResourceScheduler() {
 		new ConcreteMessage(null, "group1", "a message");
 	}
 
+	/**
+	 * Test that a {@link ConcreteMessage} object cannot be created with a
+	 * <code>null</code> groupID
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testConcreteMessageCreationWithNullGroupID() {
 		ResourceScheduler scheduler = new ResourceScheduler();
@@ -20,6 +30,10 @@ public class ConcreteMessageTest {
 
 	}
 
+	/**
+	 * Test that a {@link ConcreteMessage} object cannot be created with a
+	 * <code>null</code> content
+	 */
 	@Test(expected = NullPointerException.class)
 	public void testConcreteMessageCreationWithNullContent() {
 		ResourceScheduler scheduler = new ResourceScheduler();
@@ -27,8 +41,9 @@ public class ConcreteMessageTest {
 	}
 
 	/**
-	 * Test that a message does not throw a {@link NullPointerException} when
-	 * {@link ResourceScheduler} is not <code>null</code>
+	 * Test that creation of message does not throw a
+	 * {@link NullPointerException} when
+	 * it has no <code>null</code> parameters
 	 *
 	 * @throws NullPointerException
 	 */
